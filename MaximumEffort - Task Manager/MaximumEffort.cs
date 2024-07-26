3b0c13532b097295be9f0f518075d70f15d51d59
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-07-26 21:15:53 +0600

    Add project files.

---
+++ MaximumEffort - Task Manager/MaximumEffort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaximumEffort___Task_Manager
+{
+    public partial class MaximumEffort : Form
+    {
+        public MaximumEffort()
+        {
+            InitializeComponent();
+        }
+    }
+}
